c6e1316090d6fd12a11d640ad2da3e4ca3f953df
Harshan07-web <bharshan17@gmail.com>
2026-07-19 14:46:05 +0530

    add load logic

--- backend/app/services/load_generator.py
+++ backend/app/services/load_generator.py
@@ -1,10 +1,33 @@
 import requests
 import time 
 
-start = time.perf_counter()
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
-end = time.perf_counter()
 
-latency = end - start 
-print(f"Status Code : {response.status_code}")
-print(f"Latency :{latency*1000:.2f} ms")
+def generate_requests(tot_requests:int,url:str):
+    result = []
+    for i in range(tot_requests):
+        try:
+            start = time.perf_counter()
+            response = requests.get(url,timeout=15)
+            end = time.perf_counter()
+
+            latency = (end - start) * 1000
+
+            result.append({
+                "status_code" : response.status_code,
+                "latency_ms" : round(latency,2),
+                "success" : response.ok,
+                "error" : None
+            })
+
+        except Exception as e:
+            result.append({
+                "status_code" : None,
+                "latency_ms" : None,
+                "success" : None,
+                "error" : e
+            })
+
+    return result
+
+url = "https://jsonplaceholder.typicode.com/posts"
+print(generate_requests(10,url))
